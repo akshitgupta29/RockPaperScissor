88db3ba1a6e09095a817be1035e854454b0fc490
Akshit Gupta <akshitgupta29@gmail.com>
2018-05-11 15:22:55 +0530

    Some more validation steps.

--- Rock-Paper.py
+++ Rock-Paper.py
@@ -4,17 +4,26 @@ import utils
 #from utils import validate
 print ('Welcome to the game of Rock, Paper, Scissors')
 #print('Enter your name')
-player_name = input ('Enter your name ')
-if player_name == '' or player_name == ' ':
+player_name = utils.input_user_name()
+count = 2
+if utils.name_validate(player_name):
     print('Player name can\'t be empty.')
-    sys.exit()
-other_player_name = input('Enter the other player name (optional) ')
+    player_name = utils.input_user_name_1(count)
+    count += 1
+    if utils.name_validate(player_name):
+        print('Player name can\'t be empty.')
+        player_name = utils.input_user_name_1(count)
+        if utils.name_validate(player_name):
+            print('You have use the maximum limit. Exiting the program.')
+            sys.exit()
+
+other_player_name = input('Enter the other player name (optional): ')
 print ('Pick a hand: (0: Rock, 1: Paper, 2: Scissors)')
 
 try:
-    player_choice = int (input ('Enter your choice '))
+    player_choice = int (input ('Enter your choice: '))
 except ValueError:
-    print('Select numbers only')
+    print('Input should be a number only.')
     sys.exit()
 if utils.validate(player_choice):
     #print ('into the functions')
@@ -22,7 +31,7 @@ if utils.validate(player_choice):
     computer_hand = random.randint (0,2)
     #print(random.randint(0,2))
     utils.print_hands(player_choice, player_name)
-    if other_player_name == '' or other_player_name == ' ':
+    if utils.name_validate(other_player_name):
         utils.print_hands (computer_hand)
     else:
         utils.print_hands(computer_hand, other_player_name)
@@ -30,5 +39,5 @@ if utils.validate(player_choice):
     #print (computer_hand)   
     print (utils.judge (player_choice, computer_hand))
 else:
-    print('Enter a valid choice')
+    print('Enter a valid choice.')
 
--- utils.py
+++ utils.py
@@ -19,4 +19,16 @@ def judge (player, computer):
     else:
         return 'Win'
 
+def input_user_name():
+    player_name = input ('Enter your name: ')
+    return player_name
 
+def input_user_name_1(count):
+    player_name = input ('Enter your name for the '+str(count) + ' time: ') 
+    return player_name
+    
+def name_validate(name):
+    if name == '' or name == ' ':
+        return True
+    else:
+        return False
